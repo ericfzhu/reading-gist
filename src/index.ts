48569733957b4c920d392568df5bb7deacf055a0
eric <73148494+ericzhu98@users.noreply.github.com>
2021-10-03 03:05:28 +1100

    setup initial database query

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-const { Client } = require("@notionhq/client")
+const { Client } = require("@notionhq/client");
 const dotenv = require("dotenv")
 
 dotenv.config()
-const notion = new Client({ auth: process.env.NOTION_KEY })
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASEID
+
+const MAX_LEN = 54;
+const MAX_LINES = 5;
+
+(async () => {
+    const response = await notion.databases.query({
+        database_id: databaseId,
+        filter: {
+            or: [
+                {
+                    property: 'Status',
+                    select: {
+                        equals: "Reading",
+                    },
+                },
+            ],
+        },
+        sorts: [
+            {
+                property: 'End',
+                direction: 'ascending',
+            },
+        ],
+    });
+    console.log(response);
+})();
