24cbf73d41a4419c485b6b70410577bd88dbf42c
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-04-06 12:25:25 +1000

    Python black formatting

--- main.py
+++ main.py
@@ -12,16 +12,26 @@ api = "https://api.github.com/gists"
 
 # Get the list of book titles from GoodReads via BeautifulSoup
 page = requests.get(html)
-soup = BeautifulSoup(page.content, 'html.parser')
-titles = ([result['title'] for result in soup.find_all('a', title=True, href=re.compile("/book/show/"))])
-content = '\n'.join(titles[:5])
+soup = BeautifulSoup(page.content, "html.parser")
+titles = [
+    result["title"]
+    for result in soup.find_all("a", title=True, href=re.compile("/book/show/"))
+]
+content = "\n".join(titles[:5])
 
 # Get the current filename of the gist
 response = requests.get(f'{api}/{os.environ.get("GIST_ID")}')
-filename = list(response.json()['files'].keys())[0]
+filename = list(response.json()["files"].keys())[0]
 
 # Updates the gist with the new filename and content
-requests.patch(f"https://api.github.com/gists/{os.getenv('GIST_ID')}",
-               data=json.dumps({'files': {filename: {'filename': '📚 Currently reading books', 'content':
-                   content}}}),
-               headers={'Authorization': f'token {os.getenv("GH_TOKEN")}'})
+requests.patch(
+    f"https://api.github.com/gists/{os.getenv('GIST_ID')}",
+    data=json.dumps(
+        {
+            "files": {
+                filename: {"filename": "📚 Currently reading books", "content": content}
+            }
+        }
+    ),
+    headers={"Authorization": f'token {os.getenv("GH_TOKEN")}'},
+)
